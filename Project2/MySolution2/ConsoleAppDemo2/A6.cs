bb05edd5346e96edff6df83a186088b1d0a98eb8
hbloodprince7 <61486963+hbloodprince7@users.noreply.github.com>
2022-08-22 17:01:31 +0530

    Add files via upload

---
+++ Project2/MySolution2/ConsoleAppDemo2/A6.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleAppDemo2
+{
+    public class A6
+    {
+        public static void Main()
+        {
+            var d = DateTime.Now;
+            Console.WriteLine(d);
+        }
+
+    }
+
+}
---
+++ Project2/MySolution2/ConsoleAppDemo2/Class5.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleAppDemo2
+{
+    public class A5
+    {
+        public static void Main()
+        {
+            string str;
+            Console.WriteLine("Enter the String");
+            str = Console.ReadLine();
+            Console.WriteLine(str.Length);
+        }
+
+    }
+
+}
