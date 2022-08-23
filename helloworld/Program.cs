b4197914e77296a0d8acef6f5977ba9ed87b0c27
hbloodprince7 <61486963+hbloodprince7@users.noreply.github.com>
2022-08-23 17:08:09 +0530

    Add files via upload

---
+++ helloworld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name;
+Console.WriteLine("ACCESSING AVENGERS DATABASE");
+Console.WriteLine("ENTER YOUR NAME");
+name = Console.ReadLine();
+Console.WriteLine($"Hello Mr. {name}");
